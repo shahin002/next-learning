15d7296b943ef7dc74d5d2f1d7b7e91eedda736d
Md Shahin Howlader <mdshahin0002@gmail.com>
2022-02-18 17:21:56 +0100

    remove link from a tag

--- components/Layout/includes/SideBar.jsx
+++ components/Layout/includes/SideBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from "next/link";
 
 const SideBar = () => {
     return (
@@ -8,74 +9,85 @@ const SideBar = () => {
                     <div className="sb-sidenav-menu">
                         <div className="nav">
                             <div className="sb-sidenav-menu-heading">Core</div>
-                            <a className="nav-link" href="index.html">
-                                <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
-                                Dashboard
-                            </a>
+                            <Link href={'/'}>
+                                <a className="nav-link">
+                                    <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
+                                    Dashboard
+                                </a>
+                            </Link>
                             <div className="sb-sidenav-menu-heading">Interface</div>
-                            <a className="nav-link collapsed" href="#" data-toggle="collapse"
-                               data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <Link href={'/'}><a className="nav-link collapsed" data-toggle="collapse"
+                                                data-target="#collapseLayouts" aria-expanded="false"
+                                                aria-controls="collapseLayouts">
                                 <div className="sb-nav-link-icon"><i className="fas fa-columns"></i></div>
                                 Layouts
                                 <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
-                            </a>
+                            </a></Link>
                             <div className="collapse" id="collapseLayouts" aria-labelledby="headingOne"
                                  data-parent="#sidenavAccordion">
                                 <nav className="sb-sidenav-menu-nested nav">
-                                    <a className="nav-link" href="layout-static.html">Static Navigation</a>
-                                    <a className="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                                    <Link href={'/'}><a className="nav-link">Static
+                                        Navigation</a></Link>
+                                    <Link href={'/'}><a className="nav-link">Light
+                                        Sidenav</a></Link>
                                 </nav>
                             </div>
-                            <a className="nav-link collapsed" href="#" data-toggle="collapse"
-                               data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                            <Link href={'/'}><a className="nav-link collapsed" data-toggle="collapse"
+                                                data-target="#collapsePages" aria-expanded="false"
+                                                aria-controls="collapsePages">
                                 <div className="sb-nav-link-icon"><i className="fas fa-book-open"></i></div>
                                 Pages
                                 <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down"></i></div>
-                            </a>
+                            </a></Link>
                             <div className="collapse" id="collapsePages" aria-labelledby="headingTwo"
                                  data-parent="#sidenavAccordion">
                                 <nav className="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                    <a className="nav-link collapsed" href="#" data-toggle="collapse"
-                                       data-target="#pagesCollapseAuth" aria-expanded="false"
-                                       aria-controls="pagesCollapseAuth">
-                                        Authentication
-                                        <div className="sb-sidenav-collapse-arrow"><i
-                                            className="fas fa-angle-down"></i></div>
-                                    </a>
+                                    <Link href={'/'}>
+                                        <a className="nav-link collapsed" data-toggle="collapse"
+                                           data-target="#pagesCollapseAuth" aria-expanded="false"
+                                           aria-controls="pagesCollapseAuth">
+                                            Authentication
+                                            <div className="sb-sidenav-collapse-arrow">
+                                                <i className="fas fa-angle-down"></i>
+                                            </div>
+                                        </a>
+                                    </Link>
                                     <div className="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
                                          data-parent="#sidenavAccordionPages">
                                         <nav className="sb-sidenav-menu-nested nav">
-                                            <a className="nav-link" href="login.html">Login</a>
-                                            <a className="nav-link" href="register.html">Register</a>
-                                            <a className="nav-link" href="password.html">Forgot Password</a>
+                                            <Link href={'/'}><a className="nav-link">Login</a></Link>
+                                            <Link href={'/'}><a className="nav-link"
+                                            >Register</a></Link>
+                                            <Link href={'/'}><a className="nav-link">Forgot
+                                                Password</a></Link>
                                         </nav>
                                     </div>
-                                    <a className="nav-link collapsed" href="#" data-toggle="collapse"
-                                       data-target="#pagesCollapseError" aria-expanded="false"
-                                       aria-controls="pagesCollapseError">
+                                    <Link href={'/'}><a className="nav-link collapsed" data-toggle="collapse"
+                                                        data-target="#pagesCollapseError" aria-expanded="false"
+                                                        aria-controls="pagesCollapseError">
                                         Error
                                         <div className="sb-sidenav-collapse-arrow"><i
                                             className="fas fa-angle-down"></i></div>
-                                    </a>
+                                    </a></Link>
                                     <div className="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
                                          data-parent="#sidenavAccordionPages">
                                         <nav className="sb-sidenav-menu-nested nav">
-                                            <a className="nav-link" href="401.html">401 Page</a>
-                                            <a className="nav-link" href="404.html">404 Page</a>
-                                            <a className="nav-link" href="500.html">500 Page</a>
+                                            <Link href={'/'}><a className="nav-link">401 Page</a></Link>
+                                            <Link href={'/'}><a className="nav-link">404 Page</a></Link>
+                                            <Link href={'/'}><a className="nav-link">500 Page</a></Link>
                                         </nav>
                                     </div>
                                 </nav>
                             </div>
                             <div className="sb-sidenav-menu-heading">Addons</div>
-                            <a className="nav-link" href="charts.html">
+                            <Link href={'/'}><a className="nav-link">
                                 <div className="sb-nav-link-icon"><i className="fas fa-chart-area"></i></div>
                                 Charts
-                            </a>
-                            <a className="nav-link" href="tables.html">
+                            </a></Link>
+                            <Link href={'/'}><a className="nav-link">
                                 <div className="sb-nav-link-icon"><i className="fas fa-table"></i></div>
                                 Tables
-                            </a>
+                            </a></Link>
                         </div>
                     </div>
                     <div className="sb-sidenav-footer">
@@ -85,7 +97,8 @@ const SideBar = () => {
                 </nav>
             </div>
         </>
-    );
+    )
+        ;
 };
 
 export default SideBar;
